5d2b3c9416446cffc45d534c3789981e5b12ba93
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-08-13 20:01:57 +0530

    implemented everythig, tested ok

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+const cors = require("cors");
+app.use(cors());
+
+// Serve static files from the public directory
+app.use(express.static(__dirname + "/public"));
+
+// Route for serving the main HTML page
+app.get("/", (req, res) => {
+    res.sendFile(__dirname, "public", "index.html");
+});
+
+// Route for handling POST requests
+app.post("/post", (req, res) => {
+    try {
+        const payload = req.body;
+
+        // Checking if the payload is a valid JSON object
+        if (typeof payload === "object" && payload !== null) {
+            res.status(200).json(payload);
+        } else {
+            res.status(400).json({ error: 'Invalid JSON format, Please write correct JSON format.' });
+        }
+    } catch (error) {
+        // Handleing any internal errors and send a response with status code 500
+        res.status(500).json({ error: 'Internal server error!' });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    try {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
---
+++ public/index.js
@@ -0,0 +1,59 @@
+// Initialize an empty container for holding JSON data
+let bag = "";
+
+// Getting the form element and attach a submit event listener
+const Form = document.getElementById("Form");
+Form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    // Retrieve the input data from the form
+    const inputData = document.getElementById("inputData").value;
+
+    try {
+        // Parseing the input data as JSON
+        const parseData = JSON.parse(inputData);
+
+        // POST the parsed JSON data and receive a response
+        const res = await fetch("/post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(parseData)
+        });
+        const response = await res.json();
+
+        // Storeing the response in the 'bag' variable
+        bag = response;
+
+        // Call the 'render' function to display the response
+        render(bag);
+    } catch (error) {
+        // Show an alert for invalid JSON input
+        alert("Invalid! Please write valid JSON data.");
+    }
+});
+
+// Rendering the input data in the 'formContainer'
+function render(data) {
+    const formContainer = document.getElementById("formContainer");
+    formContainer.style.display = "block";
+    formContainer.innerHTML = "";
+
+    // Creating a form element and populating it with input data
+    const form = document.createElement("form");
+    for (let key in data) {
+        const label = document.createElement("label");
+        label.innerText = key;
+        label.setAttribute("id", "label");
+
+        const input = document.createElement("input");
+        input.type = "text";
+        input.name = key;
+        input.value = data[key];
+        input.setAttribute("id", "textInput");
+
+        form.append(label, input);
+    }
+    formContainer.append(form);
+}
